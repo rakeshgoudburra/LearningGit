ef909f8ec5020217c5f4de63a63a41c8a6fb4612
Rakesh Goud Burra <Rakesh Goud Burra>
2022-09-08 23:12:03 +0530

    first Commit

---
+++ src/main/java/com/git/practice/GitPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.git.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticeProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/git/practice/controller/ControllerHello.java
@@ -0,0 +1,12 @@
+package com.git.practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerHello {
+	@GetMapping("/")
+  public String hello() {
+	  return "hello world";
+  }
+}
